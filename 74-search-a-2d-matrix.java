4962b5b20cf568d73806a4a2350155a5ee1cbf64
Aman Jain <60892044+its007aj@users.noreply.github.com>
2024-08-10 22:26:59 +0530

    Create 74-search-a-2d-matrix.java

---
+++ 74-search-a-2d-matrix.java
@@ -0,0 +1,42 @@
+/*
+https://leetcode.com/problems/search-a-2d-matrix/description/?envType=study-plan-v2&envId=top-interview-150
+*/
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int l = 0;
+        int hi = n-1;
+        int row = 0;
+
+        //find row using binary search
+        while(l<=hi){
+            row = l+(hi-l)/2;
+            System.out.println("gdhj");
+            if(matrix[row][0]<=target && matrix[row][m-1]>=target)
+                break;
+            else if(target<matrix[row][0])
+                hi--;
+            else if(target>matrix[row][m-1])
+                l++;
+        }
+
+        // find target in the selected row
+        l = 0;
+        hi = m-1;
+        int mid = 0;
+        while(l<=hi){
+            mid = l+(hi-l)/2;
+            if(matrix[row][mid]==target)
+                return true;
+            else if(matrix[row][mid]>target)
+                hi--;
+            else if(matrix[row][mid]<target)
+                l++;
+        }
+        
+        return false;
+    }
+}
