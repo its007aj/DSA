e9ad328270aa33163ce00488dcff51d7f2db4301
Aman Jain <60892044+its007aj@users.noreply.github.com>
2024-09-03 14:48:42 +0530

    Update 137-single-number-ii.java

--- 137-single-number-ii.java
+++ 137-single-number-ii.java
@@ -8,9 +8,9 @@ class Solution {
         
         int ans = 0;
 
-        for (int i = 0; i < 32; ++i) {
+        for (int i = 0; i < 32; i++) {
             int sum = 0;
-            for (final int num : nums)
+            for (int num : nums)
                 sum += num >> i & 1;
             sum %= 3;
             ans |= sum << i;
