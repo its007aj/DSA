c787fac97abcbd11258c1aa3480b661c1d0b93dd
Aman Jain <60892044+its007aj@users.noreply.github.com>
2024-10-09 22:56:44 +0530

    Create 1025-divisor-game.java

---
+++ 1025-divisor-game.java
@@ -0,0 +1,12 @@
+/*
+https://leetcode.com/problems/divisor-game/description/?envType=problem-list-v2&envId=dynamic-programming&difficulty=EASY
+*/
+
+
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0)
+            return true;
+        return false;
+    }
+}
