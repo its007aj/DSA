203a75a5f05e6c5506d4ff48558306da9b0aa1aa
Aman Jain <60892044+its007aj@users.noreply.github.com>
2025-01-27 22:41:07 +0530

    Create 860-lemonade-change.java

---
+++ 860-lemonade-change.java
@@ -0,0 +1,39 @@
+/*
+https://leetcode.com/problems/lemonade-change/description/
+*/
+
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        
+        PriorityQueue<Integer> highToLow = new PriorityQueue<>((a,b) -> b - a);
+        int[] billCount = new int[2];
+
+        billCount[0] = 0;
+        billCount[1] = 0;
+
+        for(int i=0; i<bills.length; i++){
+            int bill = bills[i];
+            if(bill>5){
+                int diff = bill-5;
+                while(diff>=10 && billCount[1]>0){
+                    diff -= 10;
+                    billCount[1]--;
+                }
+                while(diff>=5 && billCount[0]>0){
+                    diff -= 5;
+                    billCount[0]--;
+                }
+                
+                if(diff>0)
+                    return false;
+            }
+            if(bill==5)
+                billCount[0]++;
+            if(bill==10)
+                billCount[1]++;
+        }
+        
+        return true;
+
+    }
+}
